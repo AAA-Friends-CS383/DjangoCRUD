8f4a24af3afe9d70ce635eb76052b55e86c43706
chovin <chovin@users.noreply.github.com>
2026-04-29 03:08:33 +1000

    set up database

--- todo_list/models.py
+++ todo_list/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+class Todo(models.Model):
+    # null=True allows null in the field
+    # blank=True allows not giving a value at creation
+    # db_index=True creates an index on the field
+    order_index = models.IntegerField(
+        null=True, blank=True, db_index=True
+    )
+    description = models.TextField()
+    completed = models.BooleanField(default=False)
+
+    def save(self, *args, **kwargs):
+        if self.order_index is None:
+            last = Todo.objects.order_by("-order_index").first()
+            self.order_index = (last.order_index + 1) if last else 0
+
+        super().save(*args, **kwargs)
+    
+    def __str__(self):
+        return f"({'X' if self.completed else ' '}) {self.description}"
+    
+    # This specifies the default ordering 
+    # for queries of this model. 
+    # notice the class definition is inside the Todo class
+    class Meta:
+        ordering = ["order_index"]
